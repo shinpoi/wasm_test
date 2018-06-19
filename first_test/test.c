ed6d6384b9a0aa2ce1b2ff55b44efc9cfdb81a79
shinpoi <petersbw123@gmail.com>
2018-06-20 00:01:43 +0900

    update: upload image and return a filtered image.

--- first_test/test.c
+++ first_test/test.c
@@ -3,8 +3,10 @@
 
 EMSCRIPTEN_KEEPALIVE
 void testWASM(unsigned char* buffer, unsigned int len){
-	for (int i=0; i<len; i++){
+	for (int i=0; i<len; i=i+4){
 		buffer[i] = 255 - buffer[i];
+		buffer[i+1] = 255 - buffer[i+1];
+		buffer[i+2] = 255 - buffer[i+2];
 	}
 }
 
